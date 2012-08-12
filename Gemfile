de3a339586cd33197ab5ea053893272e7fb63d4c
Anders Hassis <anders.hassis@gmail.com>
2012-08-12 23:07:30 +0200

    Added databases

--- Gemfile
+++ Gemfile
@@ -2,3 +2,6 @@ source :rubygems
 gem 'sinatra'
 gem 'thin'
 gem 'json'
+
+gem 'dm-postgres-adapter', :group => :production
+gem 'dm-sqlite-adapter', :group => :development
--- api.rb
+++ api.rb
@@ -1,5 +1,19 @@
 require 'sinatra'
 require 'json'
+require 'data_mapper'
+
+DataMapper.setup(:default, ENV['HEROKU_POSTGRESQL_ONYX_URL'] || "sqlite3://#{Dir.pwd}/appocean.db")
+
+class Note
+    include DataMapper::Resource
+    property :id, Serial
+    property :content, Text, :required => true
+    property :complete, Boolean, :required => true, :default => 0
+    property :created_at, DateTime
+    property :updated_at, DateTime
+end
+
+DataMapper.auto_upgrade!
 
 class API < Sinatra::Base
   get '/sushi.json' do
@@ -7,4 +21,10 @@ class API < Sinatra::Base
     
     {:sushi => ["Maguro", "Hamachi", "Uni", "Saba", "Ebi", "Sake", "Tai"]}.to_json
   end
+
+  get '/notes.json' do
+    @notes = Note.all :order => :id.desc
+    @title = 'All Notes'
+    erb :home
+end
 end
